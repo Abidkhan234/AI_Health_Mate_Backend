5f7024afc8bd9b6950e8239dc60cd42200980a77
Abidkhan234 <abidshahkhan237@gmail.com>
2025-11-11 11:28:28 +0500

    Fix case sensitivity in PDF storage path and add .gitkeep file

--- middlewares/multerMiddleware.js
+++ middlewares/multerMiddleware.js
@@ -2,7 +2,7 @@ import multer from "multer";
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, "./public/pdfs");
+        cb(null, "./Public/PDFs");
     },
     filename: (req, file, cb) => {
         const date = Date.now();
